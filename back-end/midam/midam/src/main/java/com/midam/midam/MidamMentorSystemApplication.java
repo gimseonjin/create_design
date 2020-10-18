0b278cb8e8e2209354d7adb55c3f9f1aaf048d95
gimseonjin <kimseonjin616@naver.com>
2020-10-18 15:55:21 +0900

    Ver0.01

---
+++ back-end/midam/midam/src/main/java/com/midam/midam/MidamMentorSystemApplication.java
@@ -0,0 +1,13 @@
+package com.midam.midam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidamMentorSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidamMentorSystemApplication.class, args);
+	}
+
+}
---
+++ back-end/midam/midam/src/test/java/com/midam/midam/MidamMentorSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midam.midam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidamMentorSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
